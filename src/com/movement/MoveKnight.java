2bf9ed15677fcde0396f53c09fa2a516ddd5fe2e
Furkan <13202351+ffbilsel@users.noreply.github.com>
2021-09-01 15:36:56 +0300

    Code changes at MoveKnight class

--- src/com/movement/MoveKnight.java
+++ src/com/movement/MoveKnight.java
@@ -1,16 +1,85 @@
 package com.movement;
 
+import com.game.Cell;
+import com.game.Game;
+import com.helper.PieceColor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+
 public class MoveKnight extends Move implements MoveType{
 
-    private final int x;
-    private final int y;
+    public MoveKnight(int @NotNull [] pos, PieceColor color){
+        super(pos, color);
+    }
 
-    public MoveKnight(Integer[] pos){
-        super(pos);
-        x = pos[0];
-        y = pos[1];
+    public void knight() {
+        Integer[] currentPos;
+        Cell currentCell;
+        int tempX, tempY;
+        int boardSize = Game.boardSize;
+        tempX = x - 1;
+        tempY = y - 2;
+        test(tempX, tempY, boardSize, 1, 2);
+        tempX = x + 1;
+        tempY = y - 2;
+        if(tempX < boardSize && tempY <= 0){
+            currentPos = new Integer[]{tempX, tempY};
+            currentCell = Game.board[tempX][tempY];
+            test(currentPos, currentCell);
+        }
+        tempX = x + 1;
+        tempY = y + 2;
+        if(tempX < boardSize && tempY < boardSize){
+            currentPos = new Integer[]{tempX, tempY};
+            currentCell = Game.board[tempX][tempY];
+            test(currentPos, currentCell);
+        }
+        tempX = x - 2;
+        tempY = y - 1;
+        test(tempX, tempY, boardSize, 2, 1);
+        tempX = x + 2;
+        tempY = y - 1;
+        if(tempX < boardSize && tempY >= 0){
+            currentPos = new Integer[]{tempX, tempY};
+            currentCell = Game.board[tempX][tempY];
+            test(currentPos, currentCell);
+        }
+        tempX = x + 2;
+        tempY = y + 1;
+        if(tempX < boardSize && tempY < boardSize){
+            currentPos = new Integer[]{tempX, tempY};
+            currentCell = Game.board[tempX][tempY];
+            test(currentPos, currentCell);
+        }
+    }
+
+    private void test(int tempX, int tempY, int boardSize, int i, int i2) {
+        Integer[] currentPos;
+        Cell currentCell;
+        if(tempX >= 0 && tempY >= 0){
+            currentPos = new Integer[]{tempX, tempY};
+            currentCell = Game.board[tempX][tempY];
+            test(currentPos, currentCell);
+        }
+        tempX = x - i;
+        tempY = y + i2;
+        if(tempX >= 0 && tempY <= boardSize){
+            currentPos = new Integer[]{tempX, tempY};
+            currentCell = Game.board[tempX][tempY];
+            test(currentPos, currentCell);
+        }
+    }
+
+    private void test(Integer[] currentPos, @NotNull Cell currentCell){
+        if(currentCell.isEmpty()){
+            super.getPossibleMoves().put(Game.generateHash(currentPos),true);
+        }
+        else if(currentCell.getCurrentPiece().getColor() != getColor()){
+            getPossibleMoves().put(Game.generateHash(currentPos),true);
+        }
     }
 
     @Override
-    public void move() {  }
+    public HashMap<Integer,Boolean> move() { this.knight();  return getPossibleMoves(); }
 }
